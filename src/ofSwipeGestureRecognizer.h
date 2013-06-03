462a242fe33c4b90390235a571629ebf3b0ca527
Ryan Raffa <ryan.raffa@gmail.com>
2013-06-03 19:59:33 -0400

    Updated ofSwipeGestureRecognizer

--- src/ofSwipeGestureRecognizer.h
+++ src/ofSwipeGestureRecognizer.h
@@ -11,9 +11,22 @@
 
 @interface ofSwipeGestureRecognizer : NSObject {  
     UISwipeGestureRecognizer *swipeGestureRecognizer;  
-}  
+    UISwipeGestureRecognizer *gestureUp;
+    UISwipeGestureRecognizer *gestureDown;
+    UISwipeGestureRecognizer *gestureLeft;
+    UISwipeGestureRecognizer *gestureRight;
+
+@public
+    NSInteger              section;
+
+}
+
+@property(assign,nonatomic) BOOL cancelsTouchesInView;
 
 -(id)initWithView:(UIView*)view;  
--(void)handleSwipe:(UISwipeGestureRecognizer *) gr;  
+-(void)swipeUp:(UISwipeGestureRecognizer *)gUp;
+-(void)swipeDown:(UISwipeGestureRecognizer *)gDown;
+-(void)swipeLeft:(UISwipeGestureRecognizer *)gLeft;
+-(void)swipeRight:(UISwipeGestureRecognizer *)gRight;
 
 @end
--- src/ofSwipeGestureRecognizer.m
+++ src/ofSwipeGestureRecognizer.m
@@ -8,22 +8,102 @@
 
 #import "ofSwipeGestureRecognizer.h"  
 
-@implementation ofSwipeGestureRecognizer  
+@implementation ofSwipeGestureRecognizer
+
+@synthesize cancelsTouchesInView;
 
 -(id)initWithView:(UIView*)view{  
-    if((self = [super init])){  
-        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];  
-        [view addGestureRecognizer:swipeGestureRecognizer];  
-    }  
+    if((self = [super init])){
+        
+        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+        gestureUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeUp:)];//direction is set by default.
+        gestureDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeDown:)];//need to set direction.
+        gestureLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeLeft:)];//direction is set by default.
+        gestureRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight:)];//need to set direction.
+
+        [gestureUp setDirection:(UISwipeGestureRecognizerDirectionUp)];
+        [gestureDown setDirection:(UISwipeGestureRecognizerDirectionDown)];
+        [gestureLeft setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+        [gestureRight setDirection:(UISwipeGestureRecognizerDirectionRight)];
+        
+        [view addGestureRecognizer:swipeGestureRecognizer];
+        [view addGestureRecognizer:gestureUp];
+        [view addGestureRecognizer:gestureDown];
+        [view addGestureRecognizer:gestureLeft];
+        [view addGestureRecognizer:gestureRight];
+        swipeGestureRecognizer.cancelsTouchesInView = NO;
+        gestureUp.cancelsTouchesInView = NO;
+        gestureDown.cancelsTouchesInView = NO;
+        gestureLeft.cancelsTouchesInView = NO;
+        gestureRight.cancelsTouchesInView = NO;
+        [swipeGestureRecognizer release];
+        [gestureUp release];
+        [gestureDown release];
+        [gestureLeft release];
+        [gestureRight release];
+        
+        NSLog(@"initwithView was run! \n");
+
+    }
     return self;  
 }  
 
--(void)handleSwipe:(UISwipeGestureRecognizer *) gr{  
-    if([gr state] == UIGestureRecognizerStateRecognized){  
-        NSLog(@"Swipe!");  
-    }  
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    NSLog(@"gestureRecognizer run! \n");
+    return YES;
+}
 
-}  
+// pulled from Greg's post to stackoverflow
+// http://stackoverflow.com/questions/3319209/setting-direction-for-uiswipegesturerecognizer
+
+-(void)swipeUp:(UISwipeGestureRecognizer *) gUp {
+    
+    NSLog(@"Up Swipe received. \n");//Lets you know this method was called by gesture recognizer.
+    NSLog(@"Direction is: %i \n ", gUp.direction);//Lets you know the numeric value of the gesture direction for confirmation (1=right).
+    //only interested in gesture if gesture state == changed or ended (From Paul Hegarty @ standford U
+    if ((gUp.state == UIGestureRecognizerStateChanged) ||
+        (gUp.state == UIGestureRecognizerStateEnded)) {
+        section++;
+        //do something for a UP swipe gesture.
+    }
+}
+
+-(void)swipeDown:(UISwipeGestureRecognizer *)gDown {
+    
+    NSLog(@"Down Swipe received. \n");//Lets you know this method was called by gesture recognizer.
+    NSLog(@"Direction is: %i \n", gDown.direction);//Lets you know the numeric value of the gesture direction for confirmation (1=right).
+    //only interested in gesture if gesture state == changed or ended (From Paul Hegarty @ standford U
+    if ((gDown.state == UIGestureRecognizerStateChanged) ||
+        (gDown.state == UIGestureRecognizerStateEnded)) {
+        //do something for a down swipe gesture.
+        section++;
+    }
+}
+
+-(void)swipeLeft:(UISwipeGestureRecognizer *) gLeft {
+    
+    NSLog(@"Left Swipe received. \n");//Lets you know this method was called by gesture recognizer.
+    NSLog(@"Direction is: %i \n", gLeft.direction);//Lets you know the numeric value of the gesture direction for confirmation (1=right).
+    //only interested in gesture if gesture state == changed or ended (From Paul Hegarty @ standford U
+    if ((gLeft.state == UIGestureRecognizerStateChanged) ||
+        (gLeft.state == UIGestureRecognizerStateEnded)) {
+        //do something for a left swipe gesture.
+        section++;
+    }
+}
+
+-(void)swipeRight:(UISwipeGestureRecognizer *) gRight {
+    
+    NSLog(@"Right Swipe received. \n");//Lets you know this method was called by gesture recognizer.
+    NSLog(@"Direction is: %i \n", gRight.direction);//Lets you know the numeric value of the gesture direction for confirmation (1=right).
+    //only interested in gesture if gesture state == changed or ended (From Paul Hegarty @ standford U
+    if ((gRight.state == UIGestureRecognizerStateChanged) ||
+        (gRight.state == UIGestureRecognizerStateEnded)) {
+        //do something for a right swipe gesture.
+        section++;
+    }
+}
+
 
 -(void)dealloc{  
     [swipeGestureRecognizer release];  
